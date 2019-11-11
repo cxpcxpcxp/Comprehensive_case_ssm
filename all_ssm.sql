47198621cd30bb61852d01b3f11d16a159a6003d
plantasun <plantasun@yeah.net>
2019-11-11 20:38:29 +0800

    mysql语句的最终版 所有的sql语句都在这

---
+++ all_ssm.sql
@@ -0,0 +1,271 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : ssmmysql
+Source Server Version : 50622
+Source Host           : localhost:3306
+Source Database       : ssm
+
+Target Server Type    : MYSQL
+Target Server Version : 50622
+File Encoding         : 65001
+
+Date: 2019-11-11 20:36:50
+*/
+-- 所有的数据 ssm-mysql
+-- 最终以这个为标准
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for member
+-- ----------------------------
+DROP TABLE IF EXISTS `member`;
+CREATE TABLE `member` (
+  `id` varchar(32) NOT NULL,
+  `NAME` varchar(20) DEFAULT NULL,
+  `nickname` varchar(20) DEFAULT NULL,
+  `phoneNum` varchar(20) DEFAULT NULL,
+  `email` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of member
+-- ----------------------------
+INSERT INTO `member` VALUES ('E61D65F673D54F68B0861025C69773DB', '张三', '小三', '18888888888', 'zs@163.com');
+
+-- ----------------------------
+-- Table structure for orders
+-- ----------------------------
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders` (
+  `id` varchar(32) NOT NULL,
+  `orderNum` varchar(20) NOT NULL,
+  `orderTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `peopleCount` int(11) DEFAULT NULL,
+  `orderDesc` varchar(500) DEFAULT NULL,
+  `payType` int(11) DEFAULT NULL,
+  `orderStatus` int(11) DEFAULT NULL,
+  `productId` varchar(32) DEFAULT NULL,
+  `memberId` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `orderNum` (`orderNum`),
+  KEY `productId` (`productId`),
+  KEY `memberId` (`memberId`),
+  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`productId`) REFERENCES `product` (`id`),
+  CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`memberId`) REFERENCES `member` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of orders
+-- ----------------------------
+INSERT INTO `orders` VALUES ('0E7231DC797C486290E8713CA3C6ECCC', '12345', '2019-11-11 19:42:10', '2', '没什么', '0', '1', '676C5BD1D35E429A8C2E114939C5685A', 'E61D65F673D54F68B0861025C69773DB');
+INSERT INTO `orders` VALUES ('2FF351C4AC744E2092DCF08CFD314420', '67890', '2019-11-11 19:42:10', '2', '没什么', '0', '1', '12B7ABF2A4C544568B0A7C69F36BF8B7', 'E61D65F673D54F68B0861025C69773DB');
+INSERT INTO `orders` VALUES ('3081770BC3984EF092D9E99760FDABDE', '55555', '2019-11-11 19:42:10', '2', '没什么', '0', '1', '9F71F01CB448476DAFB309AA6DF9497F', 'E61D65F673D54F68B0861025C69773DB');
+INSERT INTO `orders` VALUES ('55F9AF582D5A4DB28FB4EC3199385762', '33333', '2019-11-11 19:42:10', '2', '没什么', '0', '1', '9F71F01CB448476DAFB309AA6DF9497F', 'E61D65F673D54F68B0861025C69773DB');
+INSERT INTO `orders` VALUES ('5DC6A48DD4E94592AE904930EA866AFA', '54321', '2019-11-11 19:42:10', '2', '没什么', '0', '1', '676C5BD1D35E429A8C2E114939C5685A', 'E61D65F673D54F68B0861025C69773DB');
+INSERT INTO `orders` VALUES ('96CC8BD43C734CC2ACBFF09501B4DD5D', '22222', '2019-11-11 19:42:10', '2', '没什么', '0', '1', '12B7ABF2A4C544568B0A7C69F36BF8B7', 'E61D65F673D54F68B0861025C69773DB');
+INSERT INTO `orders` VALUES ('A0657832D93E4B10AE88A2D4B70B1A28', '98765', '2019-11-11 19:42:10', '2', '没什么', '0', '1', '12B7ABF2A4C544568B0A7C69F36BF8B7', 'E61D65F673D54F68B0861025C69773DB');
+INSERT INTO `orders` VALUES ('CA005CF1BE3C4EF68F88ABC7DF30E976', '44444', '2019-11-11 19:42:10', '2', '没什么', '0', '1', '9F71F01CB448476DAFB309AA6DF9497F', 'E61D65F673D54F68B0861025C69773DB');
+INSERT INTO `orders` VALUES ('E4DD4C45EED84870ABA83574A801083E', '11111', '2019-11-11 19:42:10', '2', '没什么', '0', '1', '12B7ABF2A4C544568B0A7C69F36BF8B7', 'E61D65F673D54F68B0861025C69773DB');
+
+-- ----------------------------
+-- Table structure for order_traveller
+-- ----------------------------
+DROP TABLE IF EXISTS `order_traveller`;
+CREATE TABLE `order_traveller` (
+  `orderId` varchar(32) NOT NULL DEFAULT '',
+  `travellerId` varchar(32) NOT NULL DEFAULT '',
+  PRIMARY KEY (`orderId`,`travellerId`),
+  KEY `travellerId` (`travellerId`),
+  CONSTRAINT `order_traveller_ibfk_1` FOREIGN KEY (`orderId`) REFERENCES `orders` (`id`),
+  CONSTRAINT `order_traveller_ibfk_2` FOREIGN KEY (`travellerId`) REFERENCES `traveller` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of order_traveller
+-- ----------------------------
+INSERT INTO `order_traveller` VALUES ('0E7231DC797C486290E8713CA3C6ECCC', '3FE27DF2A4E44A6DBC5D0FE4651D3D3E');
+INSERT INTO `order_traveller` VALUES ('2FF351C4AC744E2092DCF08CFD314420', '3FE27DF2A4E44A6DBC5D0FE4651D3D3E');
+INSERT INTO `order_traveller` VALUES ('5DC6A48DD4E94592AE904930EA866AFA', '3FE27DF2A4E44A6DBC5D0FE4651D3D3E');
+INSERT INTO `order_traveller` VALUES ('A0657832D93E4B10AE88A2D4B70B1A28', '3FE27DF2A4E44A6DBC5D0FE4651D3D3E');
+INSERT INTO `order_traveller` VALUES ('3081770BC3984EF092D9E99760FDABDE', 'EE7A71FB6945483FBF91543DBE851960');
+INSERT INTO `order_traveller` VALUES ('55F9AF582D5A4DB28FB4EC3199385762', 'EE7A71FB6945483FBF91543DBE851960');
+INSERT INTO `order_traveller` VALUES ('96CC8BD43C734CC2ACBFF09501B4DD5D', 'EE7A71FB6945483FBF91543DBE851960');
+INSERT INTO `order_traveller` VALUES ('CA005CF1BE3C4EF68F88ABC7DF30E976', 'EE7A71FB6945483FBF91543DBE851960');
+INSERT INTO `order_traveller` VALUES ('E4DD4C45EED84870ABA83574A801083E', 'EE7A71FB6945483FBF91543DBE851960');
+
+-- ----------------------------
+-- Table structure for permission
+-- ----------------------------
+DROP TABLE IF EXISTS `permission`;
+CREATE TABLE `permission` (
+  `id` varchar(32) NOT NULL,
+  `permissionName` varchar(50) DEFAULT NULL,
+  `url` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of permission
+-- ----------------------------
+INSERT INTO `permission` VALUES ('c55dabab047e11ea966300ff4ce5624c', 'user findAll', 'user/findAll.do');
+INSERT INTO `permission` VALUES ('d2ef385e047e11ea966300ff4ce5624c', 'user findById', 'user/findById.do');
+INSERT INTO `permission` VALUES ('da7f47df047e11ea966300ff4ce5624c', 'product findAll', 'product/findAll.do');
+INSERT INTO `permission` VALUES ('e160ddc1047e11ea966300ff4ce5624c', 'product findById', 'product findById.do');
+INSERT INTO `permission` VALUES ('e53fe1d2047e11ea966300ff4ce5624c', 'user save', 'user/save.do');
+
+-- ----------------------------
+-- Table structure for product
+-- ----------------------------
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE `product` (
+  `id` varchar(32) NOT NULL,
+  `productNum` varchar(50) NOT NULL,
+  `productName` varchar(50) DEFAULT NULL,
+  `cityName` varchar(50) DEFAULT NULL,
+  `DepartureTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `productPrice` double DEFAULT NULL,
+  `productDesc` varchar(255) DEFAULT NULL,
+  `productStatus` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `product` (`id`,`productNum`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of product
+-- ----------------------------
+INSERT INTO `product` VALUES ('12B7ABF2A4C544568B0A7C69F36BF8B7', 'bianyiit-003', '上海五日游', '上海', '2019-11-11 19:35:25', '1800', '魔都我来了', '0');
+INSERT INTO `product` VALUES ('676C5BD1D35E429A8C2E114939C5685A', 'bianyiit-002', '北京三日游', '北京', '2019-11-11 19:35:25', '1200', '不错的旅行', '1');
+INSERT INTO `product` VALUES ('9F71F01CB448476DAFB309AA6DF9497F', 'bianyiit-001', '北京三日游', '北京', '2019-11-11 19:35:25', '1200', '不错的旅行', '1');
+
+-- ----------------------------
+-- Table structure for role
+-- ----------------------------
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE `role` (
+  `id` varchar(32) NOT NULL,
+  `roleName` varchar(50) DEFAULT NULL,
+  `roleDesc` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of role
+-- ----------------------------
+INSERT INTO `role` VALUES ('3e4395cc047c11ea966300ff4ce5624c', 'user', 'notknow');
+INSERT INTO `role` VALUES ('47fd7f97047b11ea966300ff4ce5624c', 'admin', 'vip');
+INSERT INTO `role` VALUES ('49d9a72b047c11ea966300ff4ce5624c', 'manager', 'notknow');
+INSERT INTO `role` VALUES ('501dd22f047c11ea966300ff4ce5624c', 'guest', 'notknow');
+INSERT INTO `role` VALUES ('543fa2f4047c11ea966300ff4ce5624c', 'admin', 'vip');
+
+-- ----------------------------
+-- Table structure for role_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `role_permission`;
+CREATE TABLE `role_permission` (
+  `permissionId` varchar(32) NOT NULL DEFAULT '',
+  `roleId` varchar(32) NOT NULL DEFAULT '',
+  PRIMARY KEY (`permissionId`,`roleId`),
+  KEY `roleId` (`roleId`),
+  CONSTRAINT `role_permission_ibfk_1` FOREIGN KEY (`permissionId`) REFERENCES `permission` (`id`),
+  CONSTRAINT `role_permission_ibfk_2` FOREIGN KEY (`roleId`) REFERENCES `role` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of role_permission
+-- ----------------------------
+INSERT INTO `role_permission` VALUES ('da7f47df047e11ea966300ff4ce5624c', '3e4395cc047c11ea966300ff4ce5624c');
+INSERT INTO `role_permission` VALUES ('e53fe1d2047e11ea966300ff4ce5624c', '3e4395cc047c11ea966300ff4ce5624c');
+INSERT INTO `role_permission` VALUES ('c55dabab047e11ea966300ff4ce5624c', '47fd7f97047b11ea966300ff4ce5624c');
+INSERT INTO `role_permission` VALUES ('d2ef385e047e11ea966300ff4ce5624c', '47fd7f97047b11ea966300ff4ce5624c');
+INSERT INTO `role_permission` VALUES ('d2ef385e047e11ea966300ff4ce5624c', '49d9a72b047c11ea966300ff4ce5624c');
+INSERT INTO `role_permission` VALUES ('e53fe1d2047e11ea966300ff4ce5624c', '543fa2f4047c11ea966300ff4ce5624c');
+
+-- ----------------------------
+-- Table structure for syslog
+-- ----------------------------
+DROP TABLE IF EXISTS `syslog`;
+CREATE TABLE `syslog` (
+  `id` varchar(32) NOT NULL,
+  `visitTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `username` varchar(50) DEFAULT NULL,
+  `ip` varchar(30) DEFAULT NULL,
+  `url` varchar(50) DEFAULT NULL,
+  `executionTime` int(11) DEFAULT NULL,
+  `method` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of syslog
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for traveller
+-- ----------------------------
+DROP TABLE IF EXISTS `traveller`;
+CREATE TABLE `traveller` (
+  `id` varchar(32) NOT NULL,
+  `NAME` varchar(20) DEFAULT NULL,
+  `sex` varchar(20) DEFAULT NULL,
+  `phoneNum` varchar(20) DEFAULT NULL,
+  `credentialsType` int(11) DEFAULT NULL,
+  `credentialsNum` varchar(50) DEFAULT NULL,
+  `travellerType` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of traveller
+-- ----------------------------
+INSERT INTO `traveller` VALUES ('3FE27DF2A4E44A6DBC5D0FE4651D3D3E', '张龙', '男', '13333333333', '0', '123456789009876543', '0');
+INSERT INTO `traveller` VALUES ('EE7A71FB6945483FBF91543DBE851960', '张小龙', '男', '15555555555', '0', '987654321123456789', '1');
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` varchar(32) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `PASSWORD` varchar(50) DEFAULT NULL,
+  `phoneNum` varchar(20) DEFAULT NULL,
+  `STATUS` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+INSERT INTO `users` VALUES ('6604803c047a11ea966300ff4ce5624c', 'krisswen1@sina.cn', 'kriss1', '1231', '135450944821', '0');
+INSERT INTO `users` VALUES ('791b748c047a11ea966300ff4ce5624c', 'krisswen2@sina.cn', 'kriss2', '1232', '135450944822', '1');
+INSERT INTO `users` VALUES ('7e3e9be8047a11ea966300ff4ce5624c', 'krisswen3@sina.cn', 'kriss3', '1233', '135450944823', '0');
+INSERT INTO `users` VALUES ('831c07f2047a11ea966300ff4ce5624c', 'krisswen4@sina.cn', 'kriss4', '1234', '135450944824', '1');
+INSERT INTO `users` VALUES ('897856ef047a11ea966300ff4ce5624c', 'krisswen5@sina.cn', 'kriss5', '1235', '135450944825', '0');
+
+-- ----------------------------
+-- Table structure for users_role
+-- ----------------------------
+DROP TABLE IF EXISTS `users_role`;
+CREATE TABLE `users_role` (
+  `userId` varchar(32) NOT NULL DEFAULT '',
+  `roleId` varchar(32) NOT NULL DEFAULT '',
+  PRIMARY KEY (`userId`,`roleId`),
+  KEY `roleId` (`roleId`),
+  CONSTRAINT `users_role_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`),
+  CONSTRAINT `users_role_ibfk_2` FOREIGN KEY (`roleId`) REFERENCES `role` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of users_role
+-- ----------------------------
+INSERT INTO `users_role` VALUES ('6604803c047a11ea966300ff4ce5624c', '3e4395cc047c11ea966300ff4ce5624c');
+INSERT INTO `users_role` VALUES ('6604803c047a11ea966300ff4ce5624c', '47fd7f97047b11ea966300ff4ce5624c');
+INSERT INTO `users_role` VALUES ('7e3e9be8047a11ea966300ff4ce5624c', '47fd7f97047b11ea966300ff4ce5624c');
+INSERT INTO `users_role` VALUES ('791b748c047a11ea966300ff4ce5624c', '49d9a72b047c11ea966300ff4ce5624c');
+INSERT INTO `users_role` VALUES ('791b748c047a11ea966300ff4ce5624c', '501dd22f047c11ea966300ff4ce5624c');
+INSERT INTO `users_role` VALUES ('831c07f2047a11ea966300ff4ce5624c', '501dd22f047c11ea966300ff4ce5624c');
+INSERT INTO `users_role` VALUES ('831c07f2047a11ea966300ff4ce5624c', '543fa2f4047c11ea966300ff4ce5624c');
